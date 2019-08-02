64da447e7bc34e088f5ea28710011b559c079622
merry-9001 <739211562@qq.com>
2019-08-02 22:59:12 +0800

    base operation

---
+++ src/routers/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import moiveRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    moiveRouter,
+    mineRouter,
+    cinemaRouter,
+    {
+      path:'/*',
+      redirect:'/movie'
+    }
+  ]
+})
